d96975a8a0c712bc19b3b5e1d1ca523d7f4ac76a
Jacob Moeller <jacobmoe@gmail.com>
2013-11-30 23:51:12 -0500

    emberduck

--- emberduck/static/js/emberduck.js
+++ emberduck/static/js/emberduck.js
@@ -4,3 +4,41 @@ Emberduck = Ember.Application.create({
   LOG_VIEW_LOOKUPS: true,
   rootElement: '#MainContent'
 });
+
+Emberduck.Router.map(function() {
+    this.resource('tweets', function() {
+        this.resource('tweet', {path: ':tweet_id'});
+    });
+    this.resource('results');
+    this.resource('blog');
+});
+
+Emberduck.TweetsRoute = Ember.Route.extend({
+    model: function() {
+        return $.getJSON('/tweets/api/tweet/?format=json').then(function(data) {
+            return data.objects.map(function(tweet) {
+                tweet.message = tweet.message;
+                return tweet;
+            });
+        });
+    }
+});
+
+Emberduck.TweetRoute = Ember.Route.extend({
+    model: function(params) {
+        return $.getJSON('/tweets/api/tweet/?format=json').findBy('id', params.tweet_id);
+    }
+})
+Emberduck.TweetController = Ember.ObjectController.extend({
+    isEditing: false,
+
+    actions: {
+        edit: function() {
+            this.set('isEditing', true);
+        },
+        doneEditing: function() {
+            this.set('isEditing', false);
+        }
+    },
+});
+
